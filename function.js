a7d1ece7537616d8730e79fb3d77e90da0bc2990
manikandan <vmmass08@gmail.com>
2022-06-10 09:55:54 +0530

    add-file.

---
+++ function.js
@@ -0,0 +1,34 @@
+
+function add(){
+    a=10, b=28
+     c=a+b;
+     return c
+}
+ 
+ console.log(add());
+
+
+ function divi(){
+    
+   x=10, y=20
+    z = x/y;
+      return  z;
+}
+console.log(divi())
+
+function multi(a,b){
+    
+    c=a*b;
+    return c
+}
+multi(10 ,24 );
+console.log(c)
+
+
+function sub(a,b){
+    
+    c=a-b;
+    return c
+}
+sub(12 ,34 );
+console.log(c)
